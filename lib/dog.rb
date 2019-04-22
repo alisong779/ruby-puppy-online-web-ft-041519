44090206bc72487cebfaa2a99cde420a13aa830a
Alison Golden <alisong779@yahoo.com>
2019-04-22 17:22:40 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  @@all = []
+  attr_accessor :name 
+  
+    def initialize(name)
+      @name = name 
+      @@all << self 
+    end 
+    
+    def self.all 
+      @@all.each do |x|
+        puts "#{name}"
+    end 
+  end 
